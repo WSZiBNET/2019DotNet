eea9fc24471cce255381206041952d3720f70c51
Lukasz Banas <lukasz.banas1@gmail.com>
2020-01-11 15:23:39 +0100

    Added java script

---
+++ Strona/skrypt.js
@@ -0,0 +1,15 @@
+function zegar(){
+// alert("Zostales oszukany");
+
+var Data = new Date();
+var godziny = Data.getHours();
+var minuty = Data.getMinutes();
+var sekundy = Data.getSeconds();
+var czas = godziny;
+czas += ((minuty<10) ? ":0":":") + minuty;
+czas += ((sekundy<10) ? ":0":":") + sekundy;
+document.getElementById('zegar').innerHTML = czas;
+setTimeout("zegar()", 1000);
+}
+Window.onload=setTimeout("zegar()", 1000);
+    

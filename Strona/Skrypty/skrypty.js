6da7c1a1d78d58d89a6d523b4f1c3a73f33f4ee4
niewczas987 <niewczas987@gmail.com>
2020-01-11 15:29:09 +0100

    Strona internetowa

---
+++ Strona/Skrypty/skrypty.js
@@ -0,0 +1,13 @@
+function zegar() {
+    //alert("Komunikat");
+    var Data = new Date()
+    var godziny = Data.getHours();
+    var minuty = Data.getMinutes();
+    var sekundy = Data.getSeconds();
+    var czas = godziny;
+    czas += ((minuty < 10) ? ":0" : ":") + minuty;
+    czas += ((sekundy < 10) ? ":0" : ":") + sekundy;
+    document.getElementById('zegar').innerHTML = czas;
+    setTimeout("zegar()", 1000);
+}
+window.onload = setTimeout("zegar()", 1000);

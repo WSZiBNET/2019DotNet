bcfcbf15ce62856f09f020d61c4f6e4c1015c3ea
swiergok <kswiergo@wszib.edu.pl>
2020-01-11 15:24:05 +0100

    Added JavaScript

---
+++ Strona/skrypty.js
@@ -0,0 +1,13 @@
+function zegar(){
+    var Data = new Date();
+    var godziny = Data.getHours();
+    var minuty = Data.getMinutes();
+    var sekundy = Data.getSeconds();
+    var czas = godziny;
+    czas+=((minuty<10) ? ":0":";")+minuty;
+    czas+=((sekundy<10) ? ":0":";")+sekundy;
+    document.getElementById('zegar').innerHTML = czas;
+    setTimeout("zegar()");
+    
+}
+

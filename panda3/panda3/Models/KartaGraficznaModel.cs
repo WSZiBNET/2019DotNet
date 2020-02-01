6a4ab519ca1a1ebb1abb7c24929d5a58dcd47ed8
Kamila Malawska <mac.kamila@gmail.com>
2020-02-01 10:53:10 +0100

    modele dla podzespolow dodane

---
+++ panda3/panda3/Models/KartaGraficznaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace panda3.Models
+{
+    [Table("KartaGraficzna")]
+    public class KartaGraficznaModel
+    {
+        [Key]
+        public int ProcesorID { get; set; }
+        public string Nazwa { get; set; }
+    }
+}

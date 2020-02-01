85dd4f982126dfe724293be231e4c394ede849aa
Mateusz Calik <macalik@wszib.edu.pl>
2020-02-01 10:54:19 +0100

    Dodadno modele

---
+++ panda3/panda3/Models/KomputerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace panda3.Models
+{
+    [Table("Komputer")]
+    public class KomputerModel
+    {
+        [Key]
+        public int KomputerID { get; set; }
+        public string Model { get; set; }
+        public string Producent { get; set; }
+        public decimal Cena { get; set; }
+        public DateTime DataProdukcji { get; set; }
+        [ForeignKey("KartaGraficzna")]
+        public int KartagraficznaID { get; set; }
+        [ForeignKey("Procesor")]
+        public int ProcesorID { get; set; }
+        [ForeignKey("PlytaGlowna")]
+        public int PlytaglownaID { get; set; }
+    }
+}

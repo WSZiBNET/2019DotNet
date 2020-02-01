c5778a0e5b475c7590b7abcc9f20372b9e35098f
Student <marcin.stanuszek.pl@gmail.com>
2020-02-01 11:20:15 +0100

    Uzytkownicy i rezerwacja

---
+++ panda3/panda3/Models/RezerwacjaModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace panda3.Models
+{
+    [Table ("Rezerwacja")]
+    public class Rezerwacja
+    {
+        [Key]
+        public int RezerwacjaID { get; set; }
+        [ForeignKey("KomputerID")]
+        public int KomputerID { get; set; }
+        [ForeignKey("UzytkownikID")]
+        public int UzytkownikID { get; set; }
+        public DateTime DataRozpoczecia { get; set; }
+        public DateTime DataZakonczenia { get; set; }
+        public DateTime DataPrzedluzona { get; set; }
+
+    }
+}
--- panda3/panda3/Models/UzytkownicyModel.cs
+++ panda3/panda3/Models/UzytkownicyModel.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace panda3.Models
-{
-    [System.ComponentModel.DataAnnotations.Schema.Table("Users")]
-    public class UserModel
+    [Table("Uzytkownicy")]
+    public class UzytkownicyModel
     {
+          
        [Key]
-        public int UserId { get; set; }
+        public int UzytkownikId { get; set; }
         public string Login { get; set; }
-        public string Password { get; set; }
+        public string Haslo { get; set; }
         public string Email { get; set; }
-        public bool IsAdmin { get; set; }
+        public bool CzyAdmin { get; set; }
 
     }
 }

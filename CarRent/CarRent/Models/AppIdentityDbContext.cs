c5d5e9788c38a1148deae3e7f391e79d57e01962
bowkun <bowkun@wszib.edu.pl>
2020-02-02 16:43:06 +0100

    User Roles addition

---
+++ CarRent/CarRent/Models/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿//using microsoft.aspnetcore.identity;
+//using microsoft.aspnetcore.identity.entityframeworkcore;
+//using microsoft.entityframeworkcore;
+
+////-------------------------------------------------mój fragment------------------------------------------------------------------------
+//namespace carrent.models
+//{
+//    public class appidentitydbcontext : identitydbcontext<identityuser>
+//    //{
+//        public appidentitydbcontext(dbcontextoptions<appidentitydbcontext> options) : base(options) { }
+//    }
+//}
+////-------------------------------------------------mój fragment------------------------------------------------------------------------
--- CarRent/CarRent/Startup.cs
+++ CarRent/CarRent/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using CarRent.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CarRent.Models;
 
 namespace CarRent
 {
@@ -42,14 +43,32 @@ namespace CarRent
                options.UseSqlServer(
                    Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<IdentityUser>()
+
+            //-------------------------------------------------mój fragment------------------------------------------------------------------------ 
+
+
+
+
+            //services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:CarRentIdentity:ConnectionString"]));
+
+            //services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+
+
+
+
+
+            //-------------------------------------------------mój fragment------------------------------------------------------------------------
+
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            //            services.AddDefaultIdentity<ApplicationUser>()
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -74,6 +93,47 @@ namespace CarRent
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            //-------------------------------------------------mój fragment------------------------------------------------------------------------
+            //AddRole(serviceProvider).Wait();
+            //-------------------------------------------------mój fragment------------------------------------------------------------------------
         }
+
+        //-------------------------------------------------mój fragment------------------------------------------------------------------------
+        private async Task AddRole(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Admin", "Client" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var admin = new ApplicationUser
+            {
+                UserName = Configuration.GetSection("UserSettings")["UserEmail"],
+                Email = Configuration.GetSection("UserSettings")["UserEmail"]
+            };
+
+            string UserPassword = Configuration.GetSection("UserSettings")["UserPassword"];
+            var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
+            
+            if (_user == null)
+            {
+                    var createPowerUser = await UserManager.CreateAsync(admin, UserPassword);
+                    if (createPowerUser.Succeeded)
+                {
+                await UserManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+
+        }
+        //-------------------------------------------------mój fragment------------------------------------------------------------------------
     }
 }
